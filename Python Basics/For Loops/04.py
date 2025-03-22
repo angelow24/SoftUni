e1b50dd221c126d1d3a528bdcd0f5c6fcc2eac97
angelow24 <atangelow@gmail.com>
2025-03-22 11:06:02 +0200

    Add files via upload

--- Python Basics/For Loops/04.py
+++ Python Basics/For Loops/04.py
@@ -1,4 +1,24 @@
-number = int(input())
+lilly_age = int(input())
+washing_machine = float(input())
+toy_price = int(input())
 
-for i in range(number, 0, -1):
-    print(i)
+saved_money = 0
+money_gift = 10
+toys_number = 0
+
+for i in range(1, lilly_age + 1):
+    if i % 2 == 0:
+        saved_money += money_gift
+        money_gift += 10
+        saved_money -= 1
+    else:
+        toys_number += 1
+
+toys_money = toys_number * toy_price
+total_money_saved = toys_money + saved_money
+money_after_purchase = total_money_saved - washing_machine
+
+if total_money_saved >= washing_machine:
+    print(f'Yes! {money_after_purchase:.2f}')
+else:
+    print(f'No! {abs(money_after_purchase):.2f}')
